8813018bba47794b7ab13ebd07d7215395be8083
FimbulWinters <dakin.92@outlook.com>
2022-12-06 15:45:02 +0000

    Add message for when there are no comments and added loading holding message

--- src/Components/Comments.jsx
+++ src/Components/Comments.jsx
@@ -1,29 +1,39 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import { getReviewComments } from "../Utils/api";
+import { Loading } from "./Loading";
 
 export const Comments = ({ review_id }) => {
-  const [comments, setComments] = useState([]);
+  const [reviewComments, setReviewComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     getReviewComments(review_id).then(({ comments }) => {
-      setComments(comments);
+      setReviewComments(comments);
+      setIsLoading(false);
     });
   }, []);
+  let commentsExist = reviewComments.length ? true : false;
 
-  return (
-    <ul>
-      {comments.map((comment) => {
-        return (
-          <li key={comment.comment_id} className="card">
-            <p className="author">{comment.author}</p>
-            <p className="comment">{comment.body}</p>
-            <div>
-              <p className="votes">votes: {comment.votes}</p>
-              <button>Upvote</button>
-            </div>
-          </li>
-        );
-      })}
-    </ul>
-  );
+  if (commentsExist) {
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <ul>
+        {reviewComments.map((comment) => {
+          return (
+            <li key={comment.comment_id} className="card">
+              <p className="author">{comment.author}</p>
+              <p className="comment">{comment.body}</p>
+              <div>
+                <p className="votes">votes: {comment.votes}</p>
+                <button>Upvote</button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  } else {
+    return <p>No comments yet</p>;
+  }
 };
