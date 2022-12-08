183ded689e2d4ccc85150322751aa9ef4f896d3b
FimbulWinters <dakin.92@outlook.com>
2022-12-08 17:09:18 +0000

    add sort filters to all reviews

--- src/Components/ReviewsList.jsx
+++ src/Components/ReviewsList.jsx
@@ -1,23 +1,83 @@
 import { useEffect } from "react";
 import { useState } from "react";
 // import { useParams } from "react-router-dom";
-import { getReviews } from "../Utils/api";
-import { Link, Route, Routes } from "react-router-dom";
+import { getReviews, getSortedReviews } from "../Utils/api";
+import { Link } from "react-router-dom";
 import thumbsIcon from "../images/thumbs-up.svg";
 
 export const ReviewsList = () => {
   const [reviews, setReviews] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [sortBy, setSortBy] = useState("created at");
 
   useEffect(() => {
     getReviews().then(({ reviews }) => {
       setReviews(reviews);
     });
   }, []);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  const handleSelection = (e) => {
+    setSortBy(e.target.textContent);
+    setOpen(!open);
+  };
+
+  useEffect(() => {
+    if (sortBy === "created at") {
+      getReviews().then(({ reviews }) => {
+        setReviews(reviews);
+      });
+    } else {
+      getSortedReviews(sortBy).then(({ reviews }) => {
+        setReviews(reviews);
+      });
+    }
+  }, [sortBy]);
+
   return (
     <section className="bg-gray-300">
       <h2 className="text-center pt-4 font-bold text-3xl underline ">
         Reviews
       </h2>
+      <section className="h-10">
+        <span className=" inset-y-40 right-0 inline-flex">
+          <button
+            onClick={handleOpen}
+            className="bg-gray-700 ml-60 mr-5 p-1 rounded-lg"
+          >
+            sort by...
+          </button>
+          {open ? (
+            <ul className="bg-gray-500 menu p-1 rounded-2xl">
+              <li className="menu-item" onClick={handleSelection}>
+                owner
+              </li>
+              <li className="menu-item" onClick={handleSelection}>
+                title
+              </li>
+              <li className="menu-item" onClick={handleSelection}>
+                created at
+              </li>
+              <li className="menu-item" onClick={handleSelection}>
+                category
+              </li>
+              <li className="menu-item" onClick={handleSelection}>
+                votes
+              </li>
+              <li className="menu-item" onClick={handleSelection}>
+                designer
+              </li>
+              <li className="menu-item" onClick={handleSelection}>
+                comment count
+              </li>
+            </ul>
+          ) : null}
+        </span>
+        current sort {sortBy}
+      </section>
 
       <section>
         <ul>
--- src/Utils/api.js
+++ src/Utils/api.js
@@ -34,6 +34,13 @@ export const getUser = () => {
   });
 };
 
+export const getSortedReviews = (sortBy) => {
+  console.log(sortBy);
+  return boardGames.get(`/reviews?sort_by=${sortBy}`).then((res) => {
+    return res.data;
+  });
+};
+
 export const postComment = (review_id, body) => {
   return boardGames
     .post(`reviews/${review_id}/comments`, body)
